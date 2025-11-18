c2d1042262ad4ddfd383b0847878497a155ef993
luizunc <redgameplay33@gmail.com>
2025-11-18 16:11:54 -0300

    Feat: remover arquivos

--- src/main/java/com/concessionaria/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.concessionaria.repository;
-
-import com.concessionaria.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, String> {
-}
-
--- src/main/java/com/concessionaria/repository/VendaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.concessionaria.repository;
-
-import com.concessionaria.model.Venda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendaRepository extends JpaRepository<Venda, String> {
-}
-
